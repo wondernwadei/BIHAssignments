a500a09429e723ae4053e14e18396589645bc6e2
wondernwadei <155618698+wondernwadei@users.noreply.github.com>
2024-02-27 06:49:27 +0100

    Update deploy-pricefeed.js

--- Hardhat-Practice/deploy-pricefeed.js
+++ Hardhat-Practice/deploy-pricefeed.js
@@ -1,1 +1,31 @@
+const hre = require("hardhat");
+
+const API_KEY = process.env.API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const contract = require("../artifacts/contracts/PriceFeed.sol/PriceConsumer.json");
+
+const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+
+async function main() {
+  
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Token = await ethers.getContractFactory("PriceConsumer");
+  const token = await Token.deploy();
+
+  console.log("Token address:", token.address);
+ 
+  let price = await token.getETHUSD();
+  console.log(`Eth price is: ${price}`);
+}
+
+main();
+
+
 olloha
