bf050b6925534aae16f2c3499cfe9a43a96be680
wondernwadei <155618698+wondernwadei@users.noreply.github.com>
2024-03-02 13:54:56 +0100

    Update index.js

--- Nodejs-Practice/index.js
+++ Nodejs-Practice/index.js
@@ -1,1 +1,8 @@
-.,
+let http = require('http');
+
+http.createServer(function (req, res) {
+  res.write('My first Node.js server');
+  res.end();
+}).listen(7000, function(){
+ console.log("server started at 7000");
+});
